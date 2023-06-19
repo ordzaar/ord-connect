49c190d8ec75f979485c730fd863b05010bb0291
KevinK <kevin.kihata@gmail.com>
2023-06-19 16:50:42 +0800

    feat(ordit): bundle ordit-sdk and its dependencies together

---
+++ packages/sado-connect/src/dependency.js
@@ -0,0 +1,20 @@
+'use client'
+
+import ecc from '../node_modules/ordit/js/ecc'
+import bip32 from '../node_modules/ordit/js/bip32.js'
+import bip39 from '../node_modules/ordit/js/bip39.js'
+import buffer from '../node_modules/ordit/js/buffer.js'
+import btctap from '../node_modules/ordit/js/bitcoin-tap.js'
+
+
+window.bitcointp = btctap
+window.ecc = ecc
+window.bip32 = {BIP32Factory: bip32}
+window.bip39 = bip39
+window.Buffer = buffer.Buffer
+
+// needs to be awaited because all other imports must be assigned to window before we can import ordit-sdk
+import('../node_modules/ordit/js/ordit/ordit-sdk.js').then((res => {
+    window.ordit = res
+    return res
+}))
