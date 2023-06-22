c7618b3be375c32248ffd63eac6db0dc6480810e
KevinK <kevin.kihata@gmail.com>
2023-06-22 16:14:14 +0800

    add sats + fix global var missing

--- packages/sado-connect/src/dependency.js
+++ packages/sado-connect/src/dependency.js
@@ -5,13 +5,15 @@ import bip32 from './ordit-sdk/bip32.js'
 import bip39 from './ordit-sdk/bip39.js'
 import buffer from './ordit-sdk/buffer.js'
 import btctap from './ordit-sdk/bitcoin-tap.js'
-
+import * as sats from "./ordit-sdk/sats.js";
 
 window.bitcointp = btctap
 window.ecc = ecc
 window.bip32 = { BIP32Factory: bip32 }
 window.bip39 = bip39
 window.Buffer = buffer.Buffer
+window.f=0
+window.a=0
 
 // needs to be awaited because all other imports must be assigned to window before we can import ordit-sdk
 import('./ordit-sdk/ordit-sdk.js').then((res => {
