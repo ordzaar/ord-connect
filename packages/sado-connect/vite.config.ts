933f65f0bdba76c482332f62bc31971b424627c5
Jordan Kee <jordan.kee@cakedefi.com>
2023-06-26 16:17:23 +0800

    Solve buffer externalized dependency bug

--- packages/sado-connect/vite.config.ts
+++ packages/sado-connect/vite.config.ts
@@ -4,9 +4,27 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import * as packageJson from "./package.json";
 import commonjs from "@rollup/plugin-commonjs";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                process: true,
+                buffer: true
+            }),
+            NodeModulesPolyfillPlugin()
+        ]
+    }
+},
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -15,13 +33,13 @@ export default defineConfig({
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
-    }
+    },
   },
   plugins: [
     react(),
     dts({
       insertTypesEntry: true,
     }),
-    commonjs()
+    commonjs(),
   ],
 });
