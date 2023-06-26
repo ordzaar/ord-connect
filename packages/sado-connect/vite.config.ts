16ea80aba2f99188f333e69b4f8d358fec653414
Jordan Kee <jordan.kee@cakedefi.com>
2023-06-26 17:12:27 +0800

    Remove unused process polyfill

--- packages/sado-connect/vite.config.ts
+++ packages/sado-connect/vite.config.ts
@@ -4,27 +4,24 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import * as packageJson from "./package.json";
 import commonjs from "@rollup/plugin-commonjs";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-            global: 'globalThis'
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                process: true,
-                buffer: true
-            }),
-            NodeModulesPolyfillPlugin()
-        ]
-    }
-},
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({ buffer: true }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
