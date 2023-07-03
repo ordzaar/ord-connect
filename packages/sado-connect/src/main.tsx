246ae7980587d7f8015f7c1ccb53b7c2b44b4034
Jordan Kee <jordan.kee@cakedefi.com>
2023-07-03 11:50:00 +0800

    Move sample placement and background from component to example

--- packages/sado-connect/src/main.tsx
+++ packages/sado-connect/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { SadoConnectKit } from "./index.ts";
 import { AddressProvider } from "./providers/AddressContext.tsx";
+import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
