d22ffc59eeaaca43245e9bf9fcf2a1a7fa5fd60d
Jordan Kee <jordan.kee@cakedefi.com>
2023-07-09 15:22:23 +0800

    SampleComponent that displays error(s) and resulting txId

--- packages/sado-connect/src/main.tsx
+++ packages/sado-connect/src/main.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import useSend from "./hooks/useSend";
 import { SadoConnectKit } from "./index.ts";
 import { SadoConnectProvider } from "./providers/SadoContext.tsx";
 import "./style.css";
 
+function SampleComponent() {
+  const [send, error] = useSend();
+  const [result, setResult] = useState("");
+
+  return (
+    <div>
+      <span>{result && `Transaction ID: ${result}`}</span>
+      <span>{error && `Error: ${error}`}</span>
+      <button
+        onClick={async () => {
+          const txId = await send(
+            "tb1qzxtxwhsqkh0yp6ne0mpefu99gn49a945m9hc28",
+            1,
+            1
+          );
+          if (typeof txId === "string") setResult(txId);
+        }}
+      >
+        Send money
+      </button>
+    </div>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <SadoConnectProvider>
+      <SampleComponent />
       <SadoConnectKit />
     </SadoConnectProvider>
   </React.StrictMode>
