b29745f94ab4325ea9b4e20c6ff2cf5ff0f33dc9
Jordan Kee <jordan.kee@cakedefi.com>
2023-07-13 00:16:46 +0800

    Export SelectWalletModal

--- packages/sado-connect/src/index.ts
+++ packages/sado-connect/src/index.ts
@@ -1,4 +1,5 @@
 export { SadoConnectKit } from "./components/SadoConnectKit";
 export type { SadoConnectKitProp } from "./components/SadoConnectKit";
+export { SelectWalletModal } from "./components/SelectWalletModal";
 export { SadoConnectProvider, useSadoContext } from "./providers/SadoContext";
 import "./components/style.css";
