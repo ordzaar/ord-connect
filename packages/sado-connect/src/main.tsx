6c591d89215b3daa0a11a08aa71f3298223e9e13
Jordan Kee <jordan.kee@cakedefi.com>
2023-07-21 13:34:50 +0800

    Display connected address in vite sample

--- packages/sado-connect/src/main.tsx
+++ packages/sado-connect/src/main.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { useSend } from "./hooks/useSend";
 import { SadoConnectKit } from "./index";
-import { SadoConnectProvider } from "./providers/SadoContext";
+import { SadoConnectProvider, useSadoContext } from "./providers/SadoContext";
 import "./style.css";
 
 function SampleComponent() {
   const [send, error, loading] = useSend();
   const [result, setResult] = useState("");
+  const { address } = useSadoContext();
+
+  useEffect(() => {
+    console.log(address);
+  }, [address]);
 
   return (
     <div>
+      <span>{address && `Connected Address: ${address}`}</span>
       <span>{result && `Transaction ID: ${result}`}</span>
       <span>{error && `Error: ${error}`}</span>
       <span>{loading && `Loading`}</span>
