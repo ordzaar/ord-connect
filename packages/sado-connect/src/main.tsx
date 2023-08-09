33f9d11f64e3d48f4b31ac3b104853b9dda72449
Jordan Kee <jordan.kee@cakedefi.com>
2023-08-09 18:26:21 +0800

    Update examples

--- packages/sado-connect/src/main.tsx
+++ packages/sado-connect/src/main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { useBalance } from "./hooks/useBalance";
 import { useSend } from "./hooks/useSend";
-import { SadoConnectKit } from "./index";
+import { SadoConnectKit, useSign } from "./index";
 import { SadoConnectProvider, useSadoContext } from "./providers/SadoContext";
 import "./style.css";
 
 function SampleComponent() {
   const [send, error, loading] = useSend();
   const [getBalance] = useBalance();
+  const [sign] = useSign();
   const [result, setResult] = React.useState("");
   const [balance, setBalance] = React.useState(0);
 
@@ -25,8 +26,8 @@ function SampleComponent() {
         onClick={async () => {
           const txId = await send(
             "tb1qgypdud5xr0x0wugf5yv62z03ytkwxusjwsr9kq",
-            1500,
-            3
+            1,
+            2
           );
           if (typeof txId === "string") setResult(txId);
         }}
@@ -41,13 +42,23 @@ function SampleComponent() {
       >
         Check balance
       </button>
+      <button
+        onClick={async () => {
+          const signed = await sign(
+            "cHNidP8BAOwCAAAABGfgUXKNJK+kZbVZrXNL2f3cwQMckXYiYLGRyYfslcqiAQAAAAD9////0apYlECeWk3SJkG5Jbuu4Eka/wxSPbtd7bKsKzkdVtoAAAAAAP3///9v0E+Jtq2ArOI2wSTYi3gDHjddyx4GhvZuMNFQ5lTU8wAAAAAA/f///7e2GgiSx8OGF/FH60TqPZDiHuBrS6s6t8vN94fuH3KuAAAAAAD9////AgEAAAAAAAAAFgAUQQLeNoYbzPdxCaEZpQnxIuzjchLXaOgAAAAAABYAFEEC3jaGG8z3cQmhGaUJ8SLs43ISAAAAAAABAR+aZegAAAAAABYAFEEC3jaGG8z3cQmhGaUJ8SLs43ISAAEBH9wFAAAAAAAAFgAUQQLeNoYbzPdxCaEZpQnxIuzjchIAAQEfAQAAAAAAAAAWABRBAt42hhvM93EJoRmlCfEi7ONyEgABAR8BAAAAAAAAABYAFEEC3jaGG8z3cQmhGaUJ8SLs43ISAAAA"
+          );
+          console.log(signed);
+        }}
+      >
+        Sign PSBT
+      </button>
     </div>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SadoConnectProvider initialNetwork={"testnet"} initialSafeMode={false}>
+    <SadoConnectProvider initialNetwork={"testnet"} initialSafeMode={true}>
       <SampleComponent />
       <SadoConnectKit />
     </SadoConnectProvider>
