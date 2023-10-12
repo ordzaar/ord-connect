e05f18bb02dcdf433a2f09535fcde72deb77ec6e
Jordan Kee <jordan.kee@cakedefi.com>
2023-10-12 10:49:01 +0800

    Update UniSat URL

--- packages/ord-connect/src/utils/constant.ts
+++ packages/ord-connect/src/utils/constant.ts
@@ -1,4 +1,4 @@
-export const UNISAT_WALLET_CHROME_EXTENSION_URL =
-  "https://www.unisat.io/download";
+// their www subdomain doesn't work lol
+export const UNISAT_WALLET_CHROME_EXTENSION_URL = "https://unisat.io/download";
 export const XVERSE_WALLET_CHROME_EXTENSION_URL =
   "https://www.xverse.app/download";
