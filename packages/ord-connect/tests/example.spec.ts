0af93766aeb55796ce62521f9c1d44d294be0d94
Jordan Kee <jordan.kee@cakedefi.com>
2023-10-30 13:29:51 +0800

    levain secret sauce

--- packages/ord-connect/tests/example.spec.ts
+++ packages/ord-connect/tests/example.spec.ts
@@ -4,5 +4,5 @@ test("has title", async ({ page }) => {
   await page.goto("http://localhost:5173");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Ord-connect/);
+  await expect(page).toHaveTitle(/Ord connect/);
 });
