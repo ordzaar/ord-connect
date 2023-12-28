bc3d223c67cce2313008ecea190af893e9fab9e3
JJ Adonis <work@josejai.me>
2023-12-28 08:20:18 +0800

    chore(ops): update (#188)
    * chore(ops): update
    
    * chore(ops): remove conflicting ts eslint with airbnb

--- packages/ord-connect/.eslintrc.cjs
+++ packages/ord-connect/.eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "../../node_modules/@ordzaar/standard-web-linter",
   ],
