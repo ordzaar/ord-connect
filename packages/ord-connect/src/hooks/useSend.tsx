69068e299a68ce1e9b2b011d4f0b9a56c3593d85
Keng Ye <40191153+kyleleow@users.noreply.github.com>
2024-01-15 10:20:34 +0800

    feature(send): add new param to control relay logic (#203)
    * refactor: relay method into a separate hook
    
    Update sample app
    
    * feat: add relay param to send hook
    
    Remove relay hook
    
    Revert sample app

--- packages/ord-connect/src/hooks/useSend.tsx
+++ packages/ord-connect/src/hooks/useSend.tsx
@@ -8,6 +8,7 @@ type SendFunction = (
   address: string,
   satoshis: number,
   feeRate: number,
+  relay?: boolean,
 ) => Promise<string | null>;
 
 export function useSend() {
@@ -16,7 +17,7 @@ export function useSend() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const send: SendFunction = useCallback(
-    async (toAddress, satoshis, feeRate) => {
+    async (toAddress, satoshis, feeRate, relay = true) => {
       setLoading(true);
       try {
         setError(null);
@@ -51,11 +52,14 @@ export function useSend() {
           psbt: psbtBuilder.toPSBT(),
         });
 
-        const datasource = new JsonRpcDatasource({ network });
-        const txId = await datasource.relay({ hex: signedPsbt.hex });
-
+        if (relay) {
+          const datasource = new JsonRpcDatasource({ network });
+          const txId = await datasource.relay({ hex: signedPsbt.hex });
+          setLoading(false);
+          return txId;
+        }
         setLoading(false);
-        return txId;
+        return signedPsbt.hex;
       } catch (err) {
         setError((err as Error).message);
         setLoading(false);
