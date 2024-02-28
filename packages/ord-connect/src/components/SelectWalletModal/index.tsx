5493a6589e49deb29a2a46df95b1db59727f3a16
JJ Adonis <work@josejai.me>
2024-02-28 17:23:14 +0800

    chore(ops): hide ME

--- packages/ord-connect/src/components/SelectWalletModal/index.tsx
+++ packages/ord-connect/src/components/SelectWalletModal/index.tsx
@@ -414,8 +414,8 @@ export function SelectWalletModal({
                         isMobileDevice={isMobile}
                         renderAvatar={renderAvatar}
                       />
-                      <hr className="horizontal-separator" />
-                      {!isMobile && (
+                      {/* <hr className="horizontal-separator" /> */}
+                      {false && (
                         <WalletButton
                           wallet={Wallet.MAGICEDEN}
                           subtitle="Coming soon on mobile browsing"
